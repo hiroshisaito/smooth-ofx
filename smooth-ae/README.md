5aa7e26025724548a754d6c5674bf249f90636c6
hiroshi@pinapics.com <hiroshi@pinapics.com>
2026-04-17 13:40:54 +0900

    Initial import

---
+++ smooth-ae/README.md
@@ -0,0 +1,28 @@
+# SMOOTH
+アニメーションのスムージングを行うAfterEffects用のプラグインです。  
+もともと有償で販売していたものをオープンソース化しました。
+
+AfterEffects CC2017で使えるように、最新のビルド環境に対応させています。  
+ライセンスはApache2.0です。派生品の開発やインハウスでのカスタマイズなどご自由にご利用ください。  
+
+## ダウンロード
+バイナリーはこちらからダウンロードできます。
+
+Mac  
+http://assets.loilo.tv/smooth/smooth.Mac.1.4.0.zip
+
+Windows  
+http://assets.loilo.tv/smooth/smooth.Win.1.4.0.zip
+
+
+## ライセンス
+Apache2.0
+
+## ビルド方法
+### Win
+Windows10 64bit / VisualStudio2017  
+AfterEffects CC2017 SDKをダウロードしてsmoothと同じ階層のディレクトリに置いてください。
+
+### Mac
+MacOSX Sierra / XCode9.0  
+AfterEffects CC2017 SDKをダウロードして、sdkとリネーム後、smoothと同じ階層のディレクトリに置いてください。
---
+++ smooth-ae/version.h
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------//
+// 作成した人   : 杉山浩二 <sugiyama@dd.namco.co.jp>
+// 作成日       : 2004年 8/19 19:50
+// コメント     : エフェクトのバージョン
+//---------------------------------------------------------------------------//
+
+#ifndef __VERSION_H_
+#define __VERSION_H_
+
+#ifdef	_DEBUG
+#define SK_STAGE_DEVELOP	(1)
+#else
+#define	SK_STAGE_RELEASE	(1)
+#endif
+
+
+#define NAME                "smooth"
+#define HP                  "https://github.com/loilo-inc/smooth"
+
+#define MAJOR_VERSION       (1)
+#define MINOR_VERSION       (4)
+#define BUILD_VERSION       (0)
+
+
+#endif
